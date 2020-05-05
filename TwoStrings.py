351eee1446e174d1c72fd3c881b28b4576d2bed2
shosseini811 <60949035+shosseini811@users.noreply.github.com>
2020-05-04 20:42:26 -0400

    Update TwoStrings.py

--- TwoStrings.py
+++ TwoStrings.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+#https://www.hackerrank.com/challenges/two-strings/problem
 
 import math
 import os
