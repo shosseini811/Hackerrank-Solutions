38deb81e05e26e911f7c204a78ec91e57cebb1c3
shosseini811 <60949035+shosseini811@users.noreply.github.com>
2020-05-04 20:41:26 -0400

    Create TwoStrings.py

---
+++ TwoStrings.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+        for j in range(len(s1)):
+            #for k in range(len(s2)):
+                if s1[j] in s2:
+                    return('YES')
+                    #counter +=1
+        return('NO')
+                      
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
